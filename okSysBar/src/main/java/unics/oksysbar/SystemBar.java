2f41a4a525b25883e41fccf161253d5fe89e2b51
luochao <super_luo@163.com>
2024-03-04 16:12:06 +0800

    发布1.0.1:调整api名称

--- okSysBar/src/main/java/unics/oksysbar/SystemBar.java
+++ okSysBar/src/main/java/unics/oksysbar/SystemBar.java
@@ -32,7 +32,6 @@ public class SystemBar {
 
     /**
      * 设置自定义系统栏处理器
-     * @param handler
      */
     public static void setSystemBarHandler(SysBarUI handler) {
         impl = handler;
@@ -50,7 +49,7 @@ public class SystemBar {
      *
      * @param color 状态栏背景色
      */
-    public static void applyImmersiveStatusBar(@NonNull Activity activity, @ColorInt int color) {
+    public static void setImmersiveStatusBar(@NonNull Activity activity, @ColorInt int color) {
         impl.setImmersiveStatusBar(activity, color);
     }
 
@@ -61,7 +60,7 @@ public class SystemBar {
      * @param color2 状态栏背景色2
      * @param ratio  比率
      */
-    public static void applyImmersiveStatusBar(
+    public static void setImmersiveStatusBar(
             @NonNull Activity activity,
             @ColorInt int color1,
             @ColorInt int color2,
@@ -75,7 +74,7 @@ public class SystemBar {
      *
      * @param color 状态栏颜色值
      */
-    public static void applyStatusBarColor(@NonNull Activity activity, @ColorInt int color) {
+    public static void setStatusBarColor(@NonNull Activity activity, @ColorInt int color) {
         impl.setStatusBarColor(activity, color);
     }
 
@@ -87,10 +86,10 @@ public class SystemBar {
      * @param color2 颜色2
      * @param ratio  比率，0-1
      */
-    public static void applyStatusBarColor(@NonNull Activity activity,
-                                           @ColorInt int color1,
-                                           @ColorInt int color2,
-                                           @FloatRange(from = 0.0, to = 1.0) float ratio
+    public static void setStatusBarColor(@NonNull Activity activity,
+                                         @ColorInt int color1,
+                                         @ColorInt int color2,
+                                         @FloatRange(from = 0.0, to = 1.0) float ratio
     ) {
         impl.setStatusBarColor(activity, color1, color2, ratio);
     }
@@ -98,7 +97,7 @@ public class SystemBar {
     /**
      * 设置沉浸式导航栏（即导航栏会覆盖内容布局），并且导航栏背景透明
      */
-    public static void applyImmersiveNavigationBar(@NonNull Activity activity) {
+    public static void setImmersiveNavigationBar(@NonNull Activity activity) {
         impl.setImmersiveNavigationBar(activity);
     }
 
@@ -107,7 +106,7 @@ public class SystemBar {
      *
      * @param color 导航栏背景色
      */
-    public static void applyImmersiveNavigationBar(@NonNull Activity activity, @ColorInt int color) {
+    public static void setImmersiveNavigationBar(@NonNull Activity activity, @ColorInt int color) {
         impl.setImmersiveNavigationBar(activity, color);
     }
 
@@ -119,10 +118,10 @@ public class SystemBar {
      * @param color2 导航栏背景色2
      * @param ratio  比率
      */
-    public static void applyImmersiveNavigationBar(@NonNull Activity activity,
-                                                   @ColorInt int color1,
-                                                   @ColorInt int color2,
-                                                   @FloatRange(from = 0.0, to = 1.0) float ratio
+    public static void setImmersiveNavigationBar(@NonNull Activity activity,
+                                                 @ColorInt int color1,
+                                                 @ColorInt int color2,
+                                                 @FloatRange(from = 0.0, to = 1.0) float ratio
     ) {
         impl.setImmersiveNavigationBar(activity, color1, color2, ratio);
     }
@@ -132,7 +131,7 @@ public class SystemBar {
      *
      * @param color 状态栏颜色值
      */
-    public static void applyNavigationBarColor(@NonNull Activity activity, @ColorInt int color) {
+    public static void setNavigationBarColor(@NonNull Activity activity, @ColorInt int color) {
         impl.setNavigationBarColor(activity, color);
     }
 
@@ -141,10 +140,10 @@ public class SystemBar {
      * 设置Navigation Bar颜色
      * see [applyStatusBarColor]
      */
-    public static void applyNavigationBarColor(@NonNull Activity activity,
-                                               @ColorInt int color1,
-                                               @ColorInt int color2,
-                                               @FloatRange(from = 0.0, to = 1.0) float ratio
+    public static void setNavigationBarColor(@NonNull Activity activity,
+                                             @ColorInt int color1,
+                                             @ColorInt int color2,
+                                             @FloatRange(from = 0.0, to = 1.0) float ratio
     ) {
         impl.setNavigationBarColor(activity, color1, color2, ratio);
     }
@@ -152,7 +151,7 @@ public class SystemBar {
     /**
      * 设置沉浸式系统栏（即状态栏和导航栏会覆盖布局内容），并且状态栏和导航栏背景透明
      */
-    public static void applyImmersiveSystemBar(@NonNull Activity activity) {
+    public static void setImmersiveSystemBar(@NonNull Activity activity) {
         impl.setImmersiveSystemBar(activity);
     }
 
@@ -161,7 +160,7 @@ public class SystemBar {
      *
      * @param color 系统栏的背景色
      */
-    public static void applyImmersiveSystemBar(@NonNull Activity activity, @ColorInt int color) {
+    public static void setImmersiveSystemBar(@NonNull Activity activity, @ColorInt int color) {
         impl.setImmersiveSystemBar(activity, color);
     }
 
@@ -169,14 +168,14 @@ public class SystemBar {
     /**
      * 设置沉浸式系统栏（即状态栏和导航栏会覆盖布局内容），并根据给定比率混合两种颜色，将得到的颜色应用到状态栏和导航栏背景，如果比率为0，则使用[color1],如果比率为0.5,则均匀混合两种颜色，如果比率为1，则使用[color2]
      *
-     * @param color1
-     * @param color2
-     * @param ratio  比率
+     * @param color1 第一个颜色
+     * @param color2 第二个颜色
+     * @param ratio  混合比率，如果比率为0，则使用[color1],如果比率为0.5,则均匀混合两种颜色，如果比率为1，则使用[color2]
      */
-    public static void applyImmersiveSystemBar(@NonNull Activity activity,
-                                               @ColorInt int color1,
-                                               @ColorInt int color2,
-                                               @FloatRange(from = 0.0, to = 1.0) float ratio
+    public static void setImmersiveSystemBar(@NonNull Activity activity,
+                                             @ColorInt int color1,
+                                             @ColorInt int color2,
+                                             @FloatRange(from = 0.0, to = 1.0) float ratio
     ) {
         impl.setSystemBarColor(activity, color1, color2, ratio);
     }
@@ -184,17 +183,17 @@ public class SystemBar {
     /*
      * 设置System Bar的颜色：即同时设置Status Bar和Navigation Bar的背景颜色
      */
-    public static void applySystemBarColor(@NonNull Activity activity, @ColorInt int color) {
+    public static void setSystemBarColor(@NonNull Activity activity, @ColorInt int color) {
         impl.setSystemBarColor(activity, color);
     }
 
     /**
      * 设置System Bar的颜色：即同时设置Status Bar和Navigation Bar的背景颜色
      */
-    public static void applySystemBarColor(@NonNull Activity activity,
-                                           @ColorInt int color1,
-                                           @ColorInt int color2,
-                                           @FloatRange(from = 0.0, to = 1.0) float ratio
+    public static void setSystemBarColor(@NonNull Activity activity,
+                                         @ColorInt int color1,
+                                         @ColorInt int color2,
+                                         @FloatRange(from = 0.0, to = 1.0) float ratio
     ) {
         impl.setSystemBarColor(activity, color1, color2, ratio);
     }
@@ -202,23 +201,23 @@ public class SystemBar {
     /**
      * 设置状态栏为浅色模式：即状态栏背景为浅色、文字为深色（比如黑色）
      */
-    public static void applyStatusBarLightMode(@NonNull Activity activity) {
+    public static void setStatusBarLightMode(@NonNull Activity activity) {
         impl.setStatusBarLightMode(activity);
     }
 
     /**
      * 设置状态栏为深色模式：即状态栏背景为深色、文字为浅色（比如白色）
      */
-    public static void applyStatusBarDarkMode(@NonNull Activity activity) {
+    public static void setStatusBarDarkMode(@NonNull Activity activity) {
         impl.setStatusBarDarkMode(activity);
     }
 
     /**
      * 混合颜色
      *
-     * @param color1
-     * @param color2
-     * @param ratio  如果比率为0，则使用[color1],如果比率为0.5,则均匀混合两种颜色，如果比率为1，则使用[color2]
+     * @param color1 第一个颜色
+     * @param color2 第二个颜色
+     * @param ratio  混合比率，如果比率为0，则使用[color1],如果比率为0.5,则均匀混合两种颜色，如果比率为1，则使用[color2]
      */
     static int blendColor(
             @ColorInt int color1,
